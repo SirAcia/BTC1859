de88d97e99c37fdf16662441387f6db7dda87e9d
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-18 18:58:59 -0400

    Initial commit to assignment 1, added basic comments

---
+++ Assignment_1_BTC1859_ZC.R
@@ -0,0 +1,10 @@
+#' BTC1859 Data Science I, Assigbnment 1 
+#' Zacehry Chan 
+
+#' -------------------------------------
+
+#' Question 1: Suppose 6 out of 15 students in a class develop influenza, 
+#' whereas there is 20% chance for a student in similar school classes 
+#' nationwide to develop influenza. Is there evidence of an excessive number 
+#' of cases in the class? That is, what is the probability of obtaining at 
+#' least 6 cases in this class if the nationwide rate holds true?
