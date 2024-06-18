d78e1a14fd6a7faa34c19b18903f2f1fe83aad3c
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-18 19:39:40 -0400

    changed graphs to line rather than points (types parameter). Also added colours to the different distributions

--- Assignment_1_BTC1859_ZC.R
+++ Assignment_1_BTC1859_ZC.R
@@ -35,26 +35,27 @@ prob_at_least_6
 #Setting sequence for x-axis, using seq func to ensure smooth line (has many, many points)
 x <- seq(from=-4,to=8,length.out = 1000)
 y_norm <- dnorm(x, mean = 0, sd = sqrt(1), log = FALSE)
-plot(x,y_norm, type = "p", main="t-distribution, degrees of freedom", xlab = "test",ylab = "test")
+plot(x,y_norm, type = "l", main="t-distribution, degrees of freedom",col = "blue", xlab = " ",ylab = " ")
 
 df_8 <- 8
 x_8 <- seq(from=-4,to=8,length.out = 1000)
 y_8 <- dt(x_8, df_8)
-points(x,y_8, type="p")
+points(x,y_8, type="l", col="red")
 
 df_4 <- 4
 x_4 <- seq(from=-4,to=8,length.out = 1000)
 y_4 <- dt(x_4, df_4)
-points(x,y_4, type="p")
+points(x,y_4, type="l", col="green")
 
 df_2 <- 2
 x_2 <- seq(from=-4,to=8,length.out = 1000)
 y_2 <- dt(x_2, df_2)
-points(x,y_2, type="p")
+points(x,y_2, type="l", col="black")
 
 df_1 <- 1
 x_1 <- seq(from=-4,to=8,length.out = 1000)
 y_1 <- dt(x_1, df_1)
-points(x,y_1, type="p")
+points(x,y_1, type="l", col="orange")
+
 
 
