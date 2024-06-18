5c5e6505ec8a0f94a57cb07a3bb622a4d1344472
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-18 19:15:57 -0400

    Added calculation for pbinom for prob of at least 6

--- Assignment_1_BTC1859_ZC.R
+++ Assignment_1_BTC1859_ZC.R
@@ -8,3 +8,13 @@
 #' nationwide to develop influenza. Is there evidence of an excessive number 
 #' of cases in the class? That is, what is the probability of obtaining at 
 #' least 6 cases in this class if the nationwide rate holds true?
+
+
+#' Using the fucntion pbinopm() as this scenario deals with desecretized, 
+#' numerical data. Also assuming taht there are only 2 states for this sickness,
+#' sick or not sick, thus fulfilling the requiremnets (&conditions) necessary 
+#' to use a binomial distribution 
+#' 
+#' Given p = 0.2, _________
+
+prob_at_least_6 <- pbinom(5,size = 15,prob = 0.2)
