e5276d6e342b44c044df16daf94bba6183e292aa
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-07-16 14:12:51 -0400

    Added and amnded comments

--- BTC1859_Assigbment3.R
+++ BTC1859_Assigbment3.R
@@ -7,88 +7,103 @@
 #' ---------------------------------------------------------------------------------------------
 
 # QUESTION 1 
-
+# Attaching library to use in analysis 
 library(UsingR)
-library(dplyr)
-library(ggplot2)
 
+# Using help window to understand variables
 ?babies
 
+# Examining the babies data set (not asked for in question, 
+# kept in for good practice to examine the data beyond reading the help window)
 summary(babies)
 
 str(babies)
 
 head(babies)
 
-str(babies)
-
+# Seeing if any NAs in the data
 anyNA(babies)
 
+#' Most notable concerns after examining the data is the max of 999 for 
+#' gestation, that the data contains only males (may be due to original data set),
+#' no NAs in the original dataframe, no use of factoers for categorical variables 
+
 attach(babies)
-
-#' Most notable concerns is the max of 999 for gestation, only males 
-#' (may be due to original data set), no NAs in the original dataframe, no use of factoers for categorical variables 
-
+# Creating dataframe containing mother's weight and babies' weight from original data
 wgt <- data.frame(baby_wgt = c(babies$wt), mother_wgt = c(babies$wt1))
 
 summary(wgt)
 
+# Already checked for extreme values earlier, creating new dataframe to address NAs
+# (listed as 999) in mother's weight. using new dataframe to ensure data integrity
 wgt1 <- wgt
 
+# New column for mother's wieght + NAs 
 wgt1$mother_wgt_alt <- c(wgt1$mother_wgt)
 
+# Converting 999 to NA as defined by help window 
 wgt1$mother_wgt_alt[wgt1$mother_wgt == 999] <- NA
-  
-head(wgt1)
 
 anyNA(wgt1$mother_wgt_alt)
 
 summary(wgt1)
 
+# Creating scatter plot between mother's weight and babies birthweight 
 x <- wgt1$mother_wgt_alt
 y <- wgt1$baby_wgt
 plot(x,y, xlab = "Mother's Pre-Birth Weight (lbs)", ylab = "Newborn Birthweight (oz)")
 title(main = "Mother's Weight vs Babies' Birthweight")
 
-#Observations? 
+# Observations are detailed in written report 
+
 
 #' ---------------------------------------------------------------------------------------------
 
 # QUESTION 2
+
+# Defining linear model 
 wgt_model <- lm(baby_wgt~mother_wgt_alt, data = wgt1)
 
+# examining p-values, r-squared, conf. intervals, etc. 
 summary(wgt_model)
-
-plot(wgt_model)
 confint(wgt_model)
+
+#plotting scatter 
+plot(wgt_model)
 
 
 #' ---------------------------------------------------------------------------------------------
 
 # QUESTION 3
+
+# Defining new data frame with same column name/predictor variable name to match linear model 
 msX <- data.frame(mother_wgt_alt = c(160))
+
+# Using predict(..., type = "p") as this is a prediction for new data with an exact value 
 predict(wgt_model, newdata = msX, int = "p", level = 0.95)
-#level argument here can be considered redundant but included for sake of readanbility & clairity 
+#level argument here can be considered redundant but included for sake of readability & clarity 
 
 #' ---------------------------------------------------------------------------------------------
 
 # QUESTION 4
+
+# Using predict(..., type = "c") as this is a prediction for an expected average mean 
 predict(wgt_model, newdata = msX, int = "c", level = 0.95)
-#level argument here can be considered redundant but included for sake of readanbility & clairity 
+#level argument here can be considered redundant but included for sake of readability & clairity 
+
 
 #' ---------------------------------------------------------------------------------------------
 
 # QUESTION 5
+# Question 5 is answered in the word document
 
-#' Given that covariance is between -1 and 1, with 1 being perfect corrwlartion, I would argue this is positive and approx. 0.5
-#' This is because the data seesm closely correlated, realtively gerouped together, with variation in either weights matching direction to variation in the other. 
-#' (i.e. when mother birthrate is smaller than average, the birthweight is smaller as well and vice-versa) however, ecspecially with the heavier pre-birthweight, this relationship is not as strong
-#' justifying the 0.5 coefficient estimate rather than a higher value. Biologically this is supported as well, if a mother is of lower weight due to nutrition or physical stature, there is 
-#' biological limitations & pressure for a smaller birthweight (i.e. less nutrients so msaller baby or genetic bias toward individuals of a smaller stature)
 
 #' ---------------------------------------------------------------------------------------------
 
 # QUESTION 6
+
+# Running a correlation test for the response and predictor variables
 wgt_corr <- cor.test(wgt1$baby_wgt, wgt1$mother_wgt_alt, alternative = "two.sided", method = "pearson", conf.level = 0.95)
+# Defining levels here is redundant as default is 0.95 but kept for clarity 
 wgt_corr
 
