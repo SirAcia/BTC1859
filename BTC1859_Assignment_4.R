5ae5a3a3398e58d8ae713398358aa41594fd8dd1
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-07-23 13:06:42 -0400

    Created file

---
+++ BTC1859_Assignment_4.R
@@ -0,0 +1,9 @@
+#' BTC1859 Data Science in Health I, Assignment 4
+#' Prof. Nicholas Mitsakakis, Summer Term, 2024
+#' Zachery Chan, 1005468012
+
+# Github repo for R: https://github.com/SirAcia/BTC1859.git
+
+# 
+
+#' ---------------------------------------------------------------------------------------------
