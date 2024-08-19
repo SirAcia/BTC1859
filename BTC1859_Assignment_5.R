3988e9b4beac1988f461680aa35915893c036201
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-08-19 18:21:59 -0400

    finished 1-3, need to fix legend

--- BTC1859_Assignment_5.R
+++ BTC1859_Assignment_5.R
@@ -1,12 +1,32 @@
+#' BTC1859 Data Science in Health I, Assignment 5
+#' Prof. Nicholas Mitsakakis, Summer Term, 2024
+#' Zachery Chan, 1005468012
+
+# Github repo for R: https://github.com/SirAcia/BTC1859.git
+
+#' ---------------------------------------------------------------------------------------------
 
 library(pwr)
 
-# Define rho values and sample sizes
+#' ---------------------------------------------------------------------------------------------
+# QUESTION 1
+
+# Examining the context of the study, it is a two-arm, paired study, observing 2 groups
+# (control/PSG and Breadth-IDX/treatment) with an initial and a follow-up 6 months later 
+
+# As the styudy is paired, rho is assumed to be contained between [0,1] (i.e. no 
+# assumption of potential negative correlation)
+
+
+# Defining rho values and sample sizes
 rho_values <- seq(0, 1, by = 0.1)
 n_vals <- seq(10,110, by = 1)
 
+# As it is paired, calculating difference in standard deviations 
 sd_diff <- 4 * sqrt(2 - rho_values)
 
+# Calculating power values at different rho values, storing in a matrix 
+# using delta = 3, and a significance level of 0.05 
 powervals <- cbind(power.t.test(n = n_vals, delta = 3, sd = (4 * sqrt(2 -0.0)), sig.level = 0.05, type = "paired")$power, 
                    power.t.test(n = n_vals, delta = 3, sd = (4 * sqrt(2 -0.1)), sig.level = 0.05, type = "paired")$power, 
                    power.t.test(n = n_vals, delta = 3, sd = (4 * sqrt(2 -0.2)), sig.level = 0.05, type = "paired")$power, 
@@ -20,31 +40,58 @@ powervals <- cbind(power.t.test(n = n_vals, delta = 3, sd = (4 * sqrt(2 -0.0)), 
                    power.t.test(n = n_vals, delta = 3, sd = (4 * sqrt(2 -1)), sig.level = 0.05, type = "paired")$power 
 )
 
+# Plotting power curves 
+# Setting blank plot 
 plot(range(n_vals), range(powervals), type = "n", xlab="sample size",ylab="power",main="power curves")
 
+# Setting colors for power curves
 colors <- c("blue", "orange", "black", "red", "green", "darkgreen", "lightgreen", "lightblue", "tomato", "darkred", "brown")
 
+# Graphing power curves 
 matlines(n_vals, powervals, type="l", lty = 1, col = colors)
 
+# Setting legend for graph 
 legend("bottomright",legend = paste("rho=",rho_values,sep=""),col = colors)
 
 
+#' ---------------------------------------------------------------------------------------------
+# QUESTION 2
 
-#Q2
+# Setting rho to different values to test for alternative possibilities 
 rho <- seq(0.1, 0.9, by = 0.1)
+
+# Initializing a vector to store delta values
 delta <- c()
 
+# Using for loop to calculate power for each rho value
 for (i in rho) {
   
   sd_diff <- 4 * sqrt(2 - i)
-  
+  # Using n =80 here as it is n per arm
   power_result <- power.t.test(n = 80, power = 0.8, sd = sd_diff, sig.level = 0.05, type = "paired")
   
   delta <- c(delta, power_result$delta)
 }
 
+# Vector of delta values 
 delta
 
-# Q3
+# Making matrix to list power for each rho 
+deltas <- cbind(rho, delta)
+
+deltas 
+
+#' ---------------------------------------------------------------------------------------------
+# QUESTION 3
+
+# Calculating sample size for a two-proportion test with power = 0.8, significance level of 0.05
 new_result <- power.prop.test(p1=0.4,p2=0.3, power = 0.8, sig.level = 0.05, alternative = "one.sided")
-9
+
+new_result 
+
+
+
+
+
+
+
